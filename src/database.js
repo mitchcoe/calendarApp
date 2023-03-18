6b68e322b1a390b41bc347ef9ae68d91ea55460a
mitchcoe <mitchellcoe09@gmail.com>
2023-03-18 16:08:04 -0500

    linting, add basic createEvent sql command as a variable

--- src/database.js
+++ src/database.js
@@ -25,12 +25,31 @@ const getEventsByYear = (request,response) => {
 const getEventsByMonth = (request,response) => {
 	const year = parseInt(request.params.month);
 	const month = parseInt(request.params.month);
-	pool.query('SELECT * FROM events WHERE EXTRACT(MONTH FROM events.date) = $1 AND EXTRACT(MONTH FROM events.date) = $2', [year, month])
+	pool.query(`SELECT * FROM events WHERE EXTRACT(MONTH FROM events.date) = $1 
+							AND EXTRACT(MONTH FROM events.date) = $2`,
+							[year, month])
 		.then(res =>  {
 			response.status(200).json(res.rows)
 		})
 		.catch(e => console.log(e.stack));
 };
+// const createEvent = `
+// 	INSERT INTO events (
+// 		title,
+// 		description,
+// 		location,
+// 		date,
+// 		start_time,
+// 		end_time
+// 	) 
+// 	VALUES(
+// 		'test_event',
+// 		'testing server and database connection',
+// 		'Austin, TX', CURRENT_DATE,
+// 		'2023-04-18 14:11:00-07',
+// 		'2023-04-18 15:00:00-07'
+// 	)
+// `;
 
 const getEventsByDay = (request,response) => {
 	const year = parseInt(request.params.month);
