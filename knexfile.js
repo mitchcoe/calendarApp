a8dcabf8347b43d9e5fda18905955898518b6083
mitchcoe <mitchellcoe09@gmail.com>
2023-03-29 14:19:06 -0500

    add knex and do initial setup

---
+++ knexfile.js
@@ -0,0 +1,53 @@
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: {
+      user: 'me',
+      host: 'localhost',
+      database: 'calendarapp',
+      password: 'Bobcats#123!',
+      port: 5432,
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  staging: {
+    client: 'pg',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'pg',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
