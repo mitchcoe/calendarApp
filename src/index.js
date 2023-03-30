c92bf52892c76c5683c1ab3f6a208a8222d3abc5
mitchcoe <mitchellcoe09@gmail.com>
2023-03-30 11:12:09 -0500

    initial setup for redux and first test

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux'
 import './index.css';
 import App from './App';
-import store from './store'
+import setupStore from './store'
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
+    <Provider store={setupStore()}>
       <App />
     </Provider>
   </React.StrictMode>
